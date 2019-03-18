05985fbe0d9613264471e98862e8c41a6823541d
Jody Lambermont <jody.lambermont@student.ehb.be>
2019-03-18 14:37:10 +0100

    Revert "Merge branch 'master' of https://github.com/MehdiBouchema/TimeSheetGit"
    This reverts commit aa788b55223ca51e28815c17c12657c912b6bdff, reversing
    changes made to f968421325c86a07b18353ae4a77eaa15fce8277.

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,33 +1,8 @@
 import { Component } from '@angular/core';
-import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
-
-  
-  constructor(public toastController: ToastController) {}
-
-  async presentToast() {
-    const toast = await this.toastController.create({
-      message: 'Timecard added!',
-      duration: 2000
-    });
-    toast.present();
-  }
-
-  async presentToastWithOptions() {
-    const toast = await this.toastController.create({
-      message: 'Successfully added timecard!',
-      showCloseButton: true,
-      position: 'top',
-      closeButtonText: 'Done'
-    });
-    toast.present();
-  }
-}
-
-
+export class Tab1Page {}
